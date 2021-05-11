ecb4d583401e6bb73221b76c272bfa7bde121584
Michael Schmitz <schmitmi@iu.edu>
2021-05-11 16:40:52 -0400

    Update Driver.py

--- Driver.py
+++ Driver.py
@@ -6,6 +6,4 @@ import random
 process1 = subprocess.Popen('python Server.py')
 time.sleep(1)     
 process2 = subprocess.Popen('python SeqReplica1.py')
-#process3 = subprocess.Popen('python Client.py')
-#process4 = subprocess.Popen('python Client.py')
-#process4 = subprocess.Popen('python Client.py')
+
